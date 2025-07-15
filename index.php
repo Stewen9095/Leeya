03503a54521d08fc12086910f57a56e7e81cec16
Camilo Ocampo <acoc1988@gmail.com>
2025-07-15 13:38:54 -0500

    Base de Leeya

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/icon.png" type="image/png">
+    <title>Leeya</title>
+</head>
+<body>
+    
+</body>
+</html>
