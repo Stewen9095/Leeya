d7f1b5a24fc6c515f34e542cb79b12557f95e971
Camilo Ocampo <acoc1988@gmail.com>
2025-08-05 18:54:46 -0500

    New Book

---
+++ newbook.php
@@ -0,0 +1,16 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Publicar libro</title>
+    <link rel="icon" href="img/icon.png"></link>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
