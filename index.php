625d462413b3074dcf12a5a99a73ff27ae92dabd
39cam <acoc1988@gmail.com>
2026-01-15 14:10:10 -0500

    Fix index panel 1 y 2

--- index.php
+++ index.php
@@ -300,32 +300,37 @@ if (isLoggedIn()) {
             main {
                 max-width: 1440px;
                 min-width: 200px;
-                width: 85%;
+                width: 92%;
                 height: auto;
                 display: flex;
-                flex-wrap: nowrap;
+                flex-direction: column;
                 margin: 3.6rem auto 0 auto;
                 justify-content: center;
                 align-items: center;
             }
 
             .panel1 {
-                width: 100%;
+                width: 82%;
                 height: 150px;
                 display: flex;
-                flex-wrap: nowrap;
+                flex-wrap: wrap;
                 align-items: stretch;
+                justify-content: center;
             }
 
             .son11 {
-                width: 50%;
+                flex: 1 0 55%;
+                max-width: 60%;
                 height: 100%;
                 display: flex;
                 flex-direction: column;
                 justify-content: center;
                 align-items: center;
                 color: #333333;
+                align-self: center;
+                justify-self: center;
                 gap: 0;
+                overflow: hidden;
 
                 p:first-child {
                     display: flex;
@@ -355,14 +360,18 @@ if (isLoggedIn()) {
             }
 
             .son12 {
-                width: 50%;
-                height: auto;
+                flex: 1 0 45%;
+                max-width: 60%;
+                height: 95%;
                 background-color: #bebebe;
                 display: flex;
                 justify-content: flex-start;
+                align-self: center;
                 border: 1px solid #64646467;
                 align-items: center;
                 border-radius: .6rem;
+                align-self: center;
+                justify-self: center;
                 box-sizing: border-box;
 
                 .imagekid {
@@ -370,9 +379,9 @@ if (isLoggedIn()) {
                     flex-direction: row;
                     justify-content: flex-start;
                     align-items: center;
-                    height: 130%;
+                    height: 140%;
                     width: auto;
-                    margin: 0 4%;
+                    margin: 0 6%;
                     background-color: transparent;
                 }
             }
@@ -389,19 +398,23 @@ if (isLoggedIn()) {
                 .panel1 {
                     display: flex;
                     flex-direction: column;
+                    flex-wrap: nowrap;
                     gap: 2rem;
+                    width: 90%;
                     height: auto;
                     margin: auto;
                     height: auto;
                 }
 
                 .son11 {
+                    flex: 0;
                     justify-content: center;
                     width: 100%;
                     margin: auto;
+                    max-width: 100%;
 
                     p:first-child {
-                        font-size: 20px;
+                        font-size: 18px;
                     }
 
                     p:last-child {
@@ -411,11 +424,13 @@ if (isLoggedIn()) {
                 }
 
                 .son12 {
+                    flex: 0;
                     width: 100%;
+                    max-width: 98%;
                     flex-direction: column;
                     border-radius: 5px;
-                    padding: 5% 0;
-                    margin: 0 auto 2rem auto;
+                    padding: 3%;
+                    margin: auto;
 
                     .imagekid {
                         display: flex;
@@ -444,14 +459,93 @@ if (isLoggedIn()) {
             </div>
         </div>
 
-
         <style>
+            .panel2 {
+                max-width: 68%;
+                width: 100%;
+                height: 330px;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: nowrap;
+                justify-content: center;
+                align-items: center;
+                margin: 1% auto 0 auto;
+                gap: 3rem;
+            }
+
+            .son21 {
+                height: 90%;
+                width: 48%;
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .son22 {
+                width: 52%;
+                height: 80%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                video {
+                    height: 85%;
+                    width: auto;
+                }
+            }
 
 
+            @media(max-width: 500px) {
+
+                .panel2 {
+                    display: flex;
+                    flex-wrap: nowrap;
+                    flex-direction: column;
+                    max-width: 90%;
+                    height: auto;
+                    margin-top: 15%;
+                    margin-bottom: 12%;
+                    align-items: center;
+                    justify-content: center;
+                    gap: 0;
+                }
+
+                .son21 {
+                    width: 100%;
+                    height: 170px;
+                    margin: 0 0 3.8rem 0;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+
+                    .twitter-tweet {
+                        margin: 0;
+                        padding: 0;
+                        align-self: center;
+                    }
+                }
+
+                .son22 {
+                    width: 100%;
+                    height: auto;
+                    justify-content: center;
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    justify-content: center;
+
+                    video {
+                        width: 92%;
+                        height: auto;
+                    }
+                }
+
+            }
         </style>
 
-        <!--         <div class="panel2">
-            <div class="tweet-wrapper">
+        <div class="panel2">
+            <div class="son21">
                 <blockquote class="twitter-tweet">
                     <p lang="en" dir="ltr">Get any book with Leeya!</p>&mdash; readleeya (@readleeya) <a
                         href="https://twitter.com/readleeya/status/1945965007847477482?ref_src=twsrc%5Etfw">July 17,
@@ -460,12 +554,14 @@ if (isLoggedIn()) {
                 <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
 
-            <video autoplay muted loop playsinline preload="auto" poster="poster.jpg">
-                <source src="vid/udbiblio.mp4" type="video/mp4">
-                Tu navegador no soporta el video HTML5.
-            </video>
+            <div class="son22">
+                <video autoplay muted loop playsinline preload="auto" poster="poster.jpg">
+                    <source src="vid/udbiblio.mp4" type="video/mp4">
+                    Tu navegador no soporta el video HTML5.
+                </video>
+            </div>
 
-        </div> -->
+        </div>
 
 
 
@@ -514,24 +610,46 @@ if (isLoggedIn()) {
 
     </main>
 
+
+    <style>
+        footer {
+            bottom: 0;
+            height: 300px;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            max-width: 1440px;
+            width: 100%;
+            height: auto;
+            justify-content: center;
+            align-items: center;
+            background-color: #d8d8d881;
+        }
+
+        .sonfooter1 {
+            width: 40%;
+            display: flex;
+            flex-wrap: nowrap;
+        }
+    </style>
+
     <!--  
     <footer>
 
-        <div class="panel1footer">
+        <div class="sonfooter1">
 
-            <img src="img/icon.png" class="iconoimg" alt="Leeya icono">
+            <img src="img/icon.png" class="imgfooter11" alt="Leeya icono">
 
-            <div class="panel1footertext">
-                <p class="shortfooter">Leeya: un espacio de acceso a la literatura y contenido bibliográfico dedicado a
-                    los estudiantes de la Universidad Distrital Francisco José de Caldas.</p>
+            <div class="textfooter11">
+                Leeya: un espacio de acceso a la literatura y contenido bibliográfico dedicado a
+                    los estudiantes de la Universidad Distrital Francisco José de Caldas.
             </div>
 
         </div>
 
-        <div class="panel2footer">
+        <div class="sonfooter1">
 
-            <p>&copy; 2025 Leeya. Todos los derechos reservados.</p>
-            <p>Un proyecto de la Universidad Distrital Francisco José de Caldas</p>
+            <div class="textfooter21">Leeya (2026): Un proyecto de la Universidad Distrital Francisco José de Caldas</div>
 
         </div>
 
